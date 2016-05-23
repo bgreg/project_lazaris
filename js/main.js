9b47811375f21a004f713c7992f703e4bfa7c8e3
Greg McGuirk <bert.mcguirk@gmail.com>
2016-05-22 17:49:25 -0700

    add more color change locations

--- js/main.js
+++ js/main.js
@@ -15,9 +15,16 @@ $(document).ready(function(){
   });
 
   $(window).scroll(function(){
-    if($("#floating-menu").offset().top > 1400 && $("#floating-menu").offset().top < 2820){
+    var offset = $("#floating-menu").offset().top
+    if(offset > 1300 && offset < 2820){
       $("#floating-menu a").css("color", "black");
-    } else {
+    } else if(offset > 3566 && offset < 4399){
+      $("#floating-menu a").css("color", "black");
+    } else if(offset > 7496 && offset < 8960){
+      $("#floating-menu a").css("color", "black");
+    } else if(offset > 9703 && offset < 9900){
+      $("#floating-menu a").css("color", "black");
+    } else{
       $("#floating-menu a").css("color", "white");
     }
   });
